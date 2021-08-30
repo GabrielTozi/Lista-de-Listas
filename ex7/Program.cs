1a3713299500bbbb9587768c35a09c9269d34b54
Aluno <Aluno@LAB2-00>
2021-08-30 13:55:00 -0300

    finalizado

---
+++ ex7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex7
+{
+    class Menu
+    {
+        public static void menu()
+        {
+            Console.WriteLine("=======================");
+            Console.WriteLine("Digite 'sair' para sair");
+            Console.WriteLine("=======================");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Num = new List<string>();
+            string cont = " ";
+            while (cont != "sair")
+            {
+                Menu.menu();
+                Console.WriteLine("\nDigite um nome");
+                cont = Console.ReadLine();
+
+                if (cont != "sair")
+                    Num.Add(cont);
+            }
+            Console.Clear();
+
+            Console.WriteLine("Digite o nome que deseja pesquisar");
+            cont = Console.ReadLine();            
+
+            if (Num.Remove(cont) == false)
+                Console.WriteLine("Falha ao excluir o nome indicado");
+            else
+                Console.WriteLine("Nome removido com sucesso");
+        }
+    }
+}
---
+++ ex8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex8
+{
+    class Menu
+    {
+        public static void menu()
+        {
+            Console.WriteLine("=======================");
+            Console.WriteLine("Digite 'sair' para sair");
+            Console.WriteLine("=======================");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Num = new List<int>();
+            string cont = " ";
+            while (cont != "sair")
+            {
+                Menu.menu();
+                Console.WriteLine("\nDigite um número");
+                cont = Console.ReadLine();
+                if (cont != "sair")
+                {
+                    Num.Add(int.Parse(cont));
+                }
+            }
+
+            Num.Sort();
+       
+            Console.Clear();
+
+            foreach (var num in Num)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
